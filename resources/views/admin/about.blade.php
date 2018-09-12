4cd97ba4f0d7b8fa3a5794d570321221eefb6928
simonecosci <simone.cosci@gmail.com>
2018-09-12 11:08:09 +0200

    added donate button

--- resources/views/admin/about.blade.php
+++ resources/views/admin/about.blade.php
@@ -3,11 +3,13 @@
         <h1>{{ config('app.name') }}</h1>
         <h2>{{ config('app.description') }}</h2>
         <a href="https://github.com/simonecosci/wms" target="_blank">Visit the Application's page on Github</a>
-        <div class="k-content">
+        <h3>
             <b>Make a donation to support this project</b><br>
             <button type="button" class="k-button" style="background: silver; font-size: 120%; font-weight: bold;" onclick="window.open('https://www.paypal.me/simonecosci');">
                 <img src="https://www.paypalobjects.com/webstatic/i/logo/rebrand/ppcom-white.svg" style="width: 136px; height: 43px">
             </button>
+        </h3>
+        <div class="k-content">
             PHP <span id="php-version"></span><br>
             Kendo <span id="kendo-version"></span><br>
             HOST IP <?php echo $_SERVER['SERVER_ADDR']; ?><br>
