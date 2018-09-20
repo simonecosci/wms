bd6ae06e62df3c1035e835c181372f5710439319
simonecosci <simone.cosci@gmail.com>
2018-09-20 13:00:28 +0200

    Fix on MvcElement view template

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(MenusTableSeeder::class);
+        //$this->call(MvcElementsTableSeeder::class);
     }
 }
