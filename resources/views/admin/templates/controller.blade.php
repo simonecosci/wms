614790142699296d030b3704f8be8fea47d90d82
Simone Cosci <simone.cosci@gmail.com>
2018-09-26 23:03:31 +0200

    fix reopenOnSave

--- resources/views/admin/templates/controller.blade.php
+++ resources/views/admin/templates/controller.blade.php
@@ -8,7 +8,11 @@ use App\Models\{{ $element->model->name }};
 class {{ $element->controller->name }} extends CrudController {
 
     protected $view = 'admin.{{ $element->view->name }}';
+
+@if ($element->view->grid->reopenOnSave)
+    protected $reopenOnSave = true;
     
+@endif
     public function __construct({{ $element->model->name }} $model) {
         $this->model = $model;
     }
