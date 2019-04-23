30abafedc9795d3e3d6b04ff174ccafba60e969b
simonecosci <simone.cosci@gmail.com>
2019-04-23 17:52:54 +0200

    fixes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,7 +62,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        return (new User)->create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
